2e5c5a378ce4a7a4cc7403881e8145a37c0e3a1c
Ilya Etingof <etingof@gmail.com>
2017-08-04 20:49:31 +0200

    0.0.11

--- runtests.sh
+++ runtests.sh
@@ -4,11 +4,7 @@ set -e
 
 export PATH=tools:$PATH
 
-# TODO: temporarily disable failing tests
-for script in cmcdump.sh crl.sh pkcs10.sh pkcs7.sh \
-              x509dump-rfc5280.sh \
-              pkcs1.sh   pkcs8.sh  x509dump.sh
-#for script in test/*.sh
+for script in test/*.sh
 do
-  test/${script}
+  ${script}
 done
