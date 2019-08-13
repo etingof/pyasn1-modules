283db3c96dfbbbc8bb9459e5923e01c04752f65e
Russ Housley <housley@vigilsec.com>
2019-08-13 17:17:52 -0400

    Add map for opentype to RFC 3565 (#55)

--- pyasn1_modules/rfc3565.py
+++ pyasn1_modules/rfc3565.py
@@ -1,3 +1,8 @@
+# This file is being contributed to pyasn1-modules software.
+#
+# Created by Russ Housley.
+# Modified by Russ Housley to add maps for use with opentypes.
+#
 # Copyright (c) 2019, Vigil Security, LLC
 # License: http://snmplabs.com/pyasn1/license.html
 #
@@ -6,6 +11,7 @@
 #
 # ASN.1 source from:
 # https://www.rfc-editor.org/rfc/rfc3565.txt
+
 
 from pyasn1.type import constraint
 from pyasn1.type import univ
@@ -20,8 +26,8 @@ class AlgorithmIdentifier(rfc5280.AlgorithmIdentifier):
 class AES_IV(univ.OctetString):
     pass
 
-
 AES_IV.subtypeSpec = constraint.ValueSizeConstraint(16, 16)
+
 
 id_aes128_CBC = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.2')
 
@@ -29,8 +35,23 @@ id_aes192_CBC = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.22')
 
 id_aes256_CBC = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.42')
 
+
 id_aes128_wrap = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.5')
 
 id_aes192_wrap = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.25')
 
 id_aes256_wrap = univ.ObjectIdentifier('2.16.840.1.101.3.4.1.45')
+
+
+# Update the Algorithm Identifier map
+
+_algorithmIdentifierMapUpdate = {
+    id_aes128_CBC: AES_IV(),
+    id_aes192_CBC: AES_IV(),
+    id_aes256_CBC: AES_IV(),
+    id_aes128_wrap: univ.Null(),
+    id_aes192_wrap: univ.Null(),
+    id_aes256_wrap: univ.Null(),
+}
+
+rfc5280.algorithmIdentifierMap.update(_algorithmIdentifierMapUpdate)
