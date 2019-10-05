a667720e3c2fb292c92a8903b9f70dd6df375b59
russhousley <housley@vigilsec.com>
2019-10-05 12:46:06 -0400

    Add support for RFC 8360

--- tests/__main__.py
+++ tests/__main__.py
@@ -62,6 +62,7 @@ suite = unittest.TestLoader().loadTestsFromNames(
      'tests.test_rfc8018.suite',
      'tests.test_rfc8103.suite',
      'tests.test_rfc8226.suite',
+     'tests.test_rfc8360.suite',
      'tests.test_rfc8398.suite',
      'tests.test_rfc8410.suite',
      'tests.test_rfc8418.suite',
