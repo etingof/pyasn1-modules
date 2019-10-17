a9139ec7641167d88c8ac418c3ec8bc5c407d588
Aliaksei Urbanski <mimworkmail@gmail.com>
2019-10-17 07:57:05 +0300

    Declare support for Python 3.8 (#86)
    Python 3.8 is there, so I believe that it would be nice
    to declare support and add tests for it on CI.
    
    Python 3.8.0 release announcement:
    https://discuss.python.org/t/python-3-8-0-is-now-available/2478
    
    These changes also:
    
     - add .venv/ to .gitignore

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: Communications
 Topic :: System :: Monitoring
 Topic :: System :: Networking :: Monitoring
