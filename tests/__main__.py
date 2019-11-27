d8d2525a4071f58f979c3cd8aee62a91ec47f491
Russ Housley <housley@vigilsec.com>
2019-11-27 16:29:41 -0500

    Add support for RFC 3370 (#114)

--- tests/__main__.py
+++ tests/__main__.py
@@ -25,6 +25,7 @@ suite = unittest.TestLoader().loadTestsFromNames(
      'tests.test_rfc3279.suite',
      'tests.test_rfc3280.suite',
      'tests.test_rfc3281.suite',
+     'tests.test_rfc3370.suite',
      'tests.test_rfc3447.suite',
      'tests.test_rfc3560.suite',
      'tests.test_rfc3565.suite',
