c45b69a942b707e5bc8c89c080cc264979ed153b
russhousley <housley@vigilsec.com>
2019-12-11 10:48:42 -0500

    Add support for RFC 5639

--- tests/__main__.py
+++ tests/__main__.py
@@ -59,6 +59,7 @@ suite = unittest.TestLoader().loadTestsFromNames(
      'tests.test_rfc5208.suite',
      'tests.test_rfc5280.suite',
      'tests.test_rfc5480.suite',
+     'tests.test_rfc5639.suite',
      'tests.test_rfc5649.suite',
      'tests.test_rfc5652.suite',
      'tests.test_rfc5751.suite',
