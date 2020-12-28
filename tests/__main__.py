4880e1de6730db9398f3496fe0eaca9a3c8c6310
russhousley <housley@vigilsec.com>
2020-12-28 12:24:23 -0500

    Add support for RFC 8894

--- tests/__main__.py
+++ tests/__main__.py
@@ -127,7 +127,8 @@ suite = unittest.TestLoader().loadTestsFromNames(
      'tests.test_rfc8696.suite',
      'tests.test_rfc8702.suite',
      'tests.test_rfc8708.suite',
-     'tests.test_rfc8769.suite']
+     'tests.test_rfc8769.suite',
+     'tests.test_rfc8894.suite']
 )
 
 
