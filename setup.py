42fe6fcbb0095b080340be0ece6545a0cfaf9f28
Christian Heimes <cheimes@redhat.com>
2022-07-11 13:51:01 +0200

    Add GitHub Actions CI, test with 3.9 to 3.11

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
 Topic :: Communications
 Topic :: System :: Monitoring
 Topic :: System :: Networking :: Monitoring
