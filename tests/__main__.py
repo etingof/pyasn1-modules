46285fe0a4fdec6025c6085e6bba0750aebe3b4a
russhousley <housley@vigilsec.com>
2022-11-17 16:21:13 -0500

    Add support for RFC 9337

--- tests/__main__.py
+++ tests/__main__.py
@@ -170,7 +170,8 @@ suite = unittest.TestLoader().loadTestsFromNames(
      'tests.test_rfc9286.suite',
      'tests.test_rfc9289.suite',
      'tests.test_rfc9323.suite',
-     'tests.test_rfc9336.suite']
+     'tests.test_rfc9336.suite',
+     'tests.test_rfc9337.suite']
 )
 
 
